45dd7976681c4a592532d948b3fcd7519f8fc6ee
15bubbles <15bubbles.15bubbles@gmail.com>
2022-01-27 00:07:45 +0100

    Add ORM, add repository

---
+++ architecture_patterns/orm.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import MetaData, Table, Column, Integer, String
+
+from architecture_patterns.model import OrderLine
+
+metadata = MetaData()
+
+order_lines = Table(
+    "order_lines",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("sku", String(255)),
+    Column("quantity", Integer, nullable=False),
+    Column("order_id", String(255)),
+)
+
+
+def start_mappers():
+    mapper(OrderLine, order_lines)
---
+++ architecture_patterns/repository.py
@@ -0,0 +1,32 @@
+import abc
+
+from architecture_patterns.model import Batch
+from sqlalchemy.orm.session import Session
+
+class AbstractRepository(abc.ABC):
+    @abc.abstractmethod
+    def add(self, batch: Batch):
+        ...
+
+    @abc.abstractmethod
+    def get(self, reference: str) -> Batch:
+        ...
+
+    @abc.abstractmethod
+    def list(self) -> list[Batch]:
+        ...
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, batch: Batch):
+        self.session.add(batch)
+        self.session.commit()
+
+    def get(self, reference: str) -> Batch:
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self) -> list[Batch]:
+        return self.session.query(Batch).all()
