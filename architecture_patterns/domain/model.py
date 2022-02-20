769362d24b1b4c7a303c866643bf155cf66638d6
15bubbles <15bubbles.15bubbles@gmail.com>
2022-02-20 20:41:36 +0100

    Add missing version number to Product aggregate

--- architecture_patterns/domain/model.py
+++ architecture_patterns/domain/model.py
@@ -65,9 +65,10 @@ def allocate(line: OrderLine, batches: list[Batch]) -> str:
 
 
 class Product:
-    def __init__(self, sku: str, batches: list[Batch]):
+    def __init__(self, sku: str, batches: list[Batch], version_number: int = 0):
         self.sku = sku
         self.batches = batches
+        self.version_number = version_number
 
     def allocate(self, line: OrderLine) -> str:
         try:
@@ -76,4 +77,5 @@ class Product:
             raise OutOfStock(f"Out of stock for sku {line.sku}")
 
         batch.allocate(line)
+        self.version_number += 1
         return batch.reference
