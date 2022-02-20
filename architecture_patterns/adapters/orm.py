06d840d55681fe7e8b6e7d1bcaa82e5c3e5119c3
15bubbles <15bubbles.15bubbles@gmail.com>
2022-02-20 20:12:47 +0100

    Add more orm models

--- architecture_patterns/adapters/orm.py
+++ architecture_patterns/adapters/orm.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import mapper
-from sqlalchemy import MetaData, Table, Column, Integer, String
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy import MetaData, Table, Column, Integer, String, ForeignKey, Date
 
-from architecture_patterns.domain.model import OrderLine
+from architecture_patterns.domain import model
 
 metadata = MetaData()
 
@@ -14,6 +14,44 @@ order_lines = Table(
     Column("order_id", String(255)),
 )
 
+products = Table(
+    "products",
+    metadata,
+    Column("sku", String(255), primary_key=True),
+    Column("version_number", Integer, nullable=False, server_default=0),
+)
+
+batches = Table(
+    "batches",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("sku", ForeignKey("products.sku")),
+    Column("reference", String(255)),
+    Column("eta", Date, nullable=True),
+)
+
+allocations = Table(
+    "allocations",
+    metadata,
+    Column("id", Integer, primary_key=True, autoincrement=True),
+    Column("order_line_id", ForeignKey("order_lines.id")),
+    Column("batch_id", ForeignKey("batches.id")),
+)
+
 
 def start_mappers():
-    mapper(OrderLine, order_lines)
+    order_lines_mapper = mapper(model.OrderLine, order_lines)
+    batches_mapper = mapper(
+        model.Batch,
+        batches,
+        properties={
+            "_allocations": relationship(
+                order_lines_mapper,
+                secondary=allocations,
+                collection_class=set,
+            )
+        },
+    )
+    mapper(
+        model.Product, products, properties={"batches": relationship(batches_mapper)}
+    )
